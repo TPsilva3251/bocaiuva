12a9c45a9c79c8bbbb0e143621e93166bb963507
thiagop127001 <thiagogua2009@gmail.com>
2022-12-04 19:16:52 -0300

    tornando menu responsivo

---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,73 @@
+<div class="relative bg-gradient-to-r from-blue-200 to-cyan-100">
+    <div class="mx-auto max-w-7xl px-4 sm:px-6">
+        <div class="flex items-center justify-between   py-6 md:justify-start md:space-x-10">
+            <div class="flex justify-start lg:w-0 lg:flex-1">
+                <a href="/home">
+                    <span class="sr-only">Portal Bocaiuva</span>
+                    <img class="w-20" src="/images/lupa.jpg" alt="">
+                </a>
+            </div>
+                <nav class="hidden space-x-10 md:flex">
+                    <a href="#" class="text-base font-medium text-gray-500 hover:text-gray-900">Home<br><i
+                            class="fa-solid fa-house text-3xl px-2"></i></i></a>
+                    <a href="#" class="text-base font-medium text-gray-500 hover:text-gray-900">Lojas<br><i
+                            class="fa-solid fa-shop text-3xl"></i></i></a>
+                    <a href="#" class="text-base font-medium text-gray-500 hover:text-gray-900">Produtos<br><i
+                            class="fa-brands fa-shopify text-3xl px-4"></i></a>
+                    <a href="#" class="text-base font-medium text-gray-500 hover:text-gray-900">Anúncios<br><i
+                            class="fa-solid fa-newspaper text-3xl px-4"></i></a>                    
+                </nav>
+            <div class="hidden items-center justify-end md:flex md:flex-1 lg:w-0">
+                <a href="#" class="text-base font-medium text-gray-500 hover:text-gray-900 ">PUBLIQUE GRÁTIS<br><i class="fa-solid fa-bullhorn text-2xl px-14"></i></a>
+                <a href="#" class="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">Sign
+                    in</a>
+                <a href="#"
+                    class="ml-8 inline-flex items-center justify-center whitespace-nowrap rounded-md border border-blue-500 hover:border-2  px-4 py-2 text-base font-medium text-gray-500 shadow-sm hover:text-gray-900">Sign
+                    up</a>
+            </div>
+        </div>
+    </div>
+
+    <!--
+      Mobile menu, show/hide based on mobile menu state.
+  
+      Entering: "duration-200 ease-out"
+        From: "opacity-0 scale-95"
+        To: "opacity-100 scale-100"
+      Leaving: "duration-100 ease-in"
+        From: "opacity-100 scale-100"
+        To: "opacity-0 scale-95"
+    -->
+    <div class="absolute inset-x-0 top-0 origin-top-right transform p-2 transition md:hidden">
+        <div class="divide-y-2 divide-gray-50 ">
+            <div class="px-5 pt-5 pb-6">
+                <div class="flex items-center justify-between">
+                    <div class="-mr-2">
+                        <a href="/home"><span class="sr-only">Portal Bocaiuva</span></a>
+                    </div>
+                    <div>
+                        <a href="#"
+                            class="flex w-full items-center justify-center rounded-md border border-blue-500 hover:border-2  px-4 py-2 text-base font-medium text-gray-500 shadow-sm hover:text-gray-900">Sign
+                            up</a>
+                        <p class="mt-6 text-center text-base font-medium text-gray-500">
+                            Existing customer?
+                            <a href="#" class="text-indigo-600 hover:text-indigo-500">Sign in</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="space-y-6 py-6 px-5">
+                <div class="flex items-center gap-y-4 gap-x-8">
+                    <a href="/home" class="text-base font-medium text-gray-500 hover:text-gray-900"><span
+                            class="">Home<br><i class="fa-solid fa-house text-2xl px-2"></i></span></a>
+                    <a href="#" class="text-base font-medium text-gray-500 hover:text-gray-900"><span
+                            class="">Lojas<br><i class="fa-solid fa-shop text-2xl"></i></span></a>
+                    <a href="#" class="text-base font-medium text-gray-500 hover:text-gray-900"><span
+                            class="">Produtos<br><i class="fa-brands fa-shopify text-2xl px-4"></i></span></a>
+                    <a href="#" class="text-base font-medium text-gray-500 hover:text-gray-900"><span
+                            class="">Anúncios<br><i class="fa-solid fa-newspaper text-2xl px-4"></i></span></a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
