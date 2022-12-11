0119df1eca3dee3e1190d23c1ff20c8569dd96c6
thiagop127001 <thiagogua2009@gmail.com>
2022-12-11 07:18:29 -0300

    trabalhando em layout home page

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,88 +1,131 @@
-<div class="relative bg-gray-100">
-    <div class="mx-auto max-w-7xl px-4 sm:px-6">
-        <div class="flex items-center justify-between   py-3 md:justify-start md:space-x-10">
-            
-                <nav class="hidden space-x-10 md:flex">
-                    <a href="#" class="text-base font-medium text-gray-500 hover:text-gray-900">Home<br></a>
-                    <a href="#" class="text-base font-medium text-gray-500 hover:text-gray-900">Lojas<br></a>
-                    <a href="#" class="text-base font-medium text-gray-500 hover:text-gray-900">Produtos<br></a>
-                    <a href="#" class="text-base font-medium text-gray-500 hover:text-gray-900">Anúncios<br></a>                    
-                </nav>
-            <div class="hidden items-center justify-end md:flex md:flex-1 lg:w-0">
-                {{-- <a href="#" class="text-base font-medium text-gray-500 hover:text-gray-900 ">PUBLIQUE GRÁTIS<br></a> --}}
-                <a href="#" class="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">Sign
-                    in</a>
-                <a href="#"
-                    class="ml-8 inline-flex items-center justify-center whitespace-nowrap rounded-md border border-blue-500   px-4 py-2 text-base font-medium text-gray-500 shadow-sm hover:text-gray-900">Sign
-                    up</a>
+<div class="absolute w-full z-10 top-0">
+    <nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5  dark:bg-gray-900">
+        <div class="container flex flex-wrap items-center justify-between mx-auto">
+            <a href="/home" class="flex items-center">
+                <img src="https://flowbite.com/docs/images/logo.svg" class="h-6 mr-3 sm:h-9" alt="Flowbite Logo" />
+                <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">PortalBocaiúva</span>
+            </a>
+            {{-- <button data-collapse-toggle="navbar-default" id="btn" onclick="open_menu()" type="button"
+                class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                aria-controls="navbar-default" aria-expanded="false">
+                <span class="sr-only">Open main menu</span>
+                <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd"
+                        d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                        clip-rule="evenodd"></path>
+                </svg>
+            </button> --}}
+            {{-- media-sm inicio --}}
+            <div class="md:hidden space-x-3">
+                <a href="#" class="text-white ">Home</a>
+                <a href="#" class="text-white ">About</a>
+                <a href="#" class="text-white ">Services</a>
+                <a href="#" class="text-white ">Pricing</a>
+                <a href="#" class="text-white ">Contact</a>
             </div>
-        </div>       
-    </div>
-
-    <!--
-      Mobile menu, show/hide based on mobile menu state.
-  
-      Entering: "duration-200 ease-out"
-        From: "opacity-0 scale-95"
-        To: "opacity-100 scale-100"
-      Leaving: "duration-100 ease-in"
-        From: "opacity-100 scale-100"
-        To: "opacity-0 scale-95"
-    -->
-    {{-- <div class="absolute inset-x-0 top-0 origin-top-right transform p-2 transition md:hidden">
-        <div class="divide-y-2 divide-gray-50 ">
-            <div class="px-5 pt-5 pb-6">
-                <div class="flex items-center justify-between">
-                    <div class="-mr-2">
-                        <a href="/home"><span class="sr-only">Portal Bocaiuva</span></a>
-                    </div>
-                    <div>
+            {{-- media-sm fim --}}
+            <div class="hidden w-full md:block md:w-auto" id="navbar-default">
+                <ul
+                    class="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                    <li>
                         <a href="#"
-                            class="flex w-full items-center justify-center rounded-md border border-blue-500 hover:border-2  px-4 py-2 text-base font-medium text-gray-500 shadow-sm hover:text-gray-900">Sign
-                            up</a>
-                        <p class="mt-6 text-center text-base font-medium text-gray-500">
-                            Existing customer?
-                            <a href="#" class="text-indigo-600 hover:text-indigo-500">Sign in</a>
-                        </p>
+                            class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white"
+                            aria-current="page">Home</a>
+                    </li>
+                    <li>
+                        <a href="#"
+                            class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About</a>
+                    </li>
+                    <li>
+                        <a href="#"
+                            class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Services</a>
+                    </li>
+                    <li>
+                        <a href="#"
+                            class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Pricing</a>
+                    </li>
+                    <li>
+                        <a href="#"
+                            class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    
+    
+    <!--
+          Mobile menu, show/hide based on mobile menu state.
+      
+          Entering: "duration-200 ease-out"
+            From: "opacity-0 scale-95"
+            To: "opacity-100 scale-100"
+          Leaving: "duration-100 ease-in"
+            From: "opacity-100 scale-100"
+            To: "opacity-0 scale-95"
+        -->
+    {{-- <div class="absolute inset-x-0 top-0 origin-top-right transform p-2 transition md:hidden">
+            <div class="divide-y-2 divide-gray-50 ">
+                <div class="px-5 pt-5 pb-6">
+                    <div class="flex items-center justify-between">
+                        <div class="-mr-2">
+                            <a href="/home"><span class="sr-only">Portal Bocaiuva</span></a>
+                        </div>
+                        <div>
+                            <a href="#"
+                                class="flex w-full items-center justify-center rounded-md border border-blue-500 hover:border-2  px-4 py-2 text-base font-medium text-gray-500 shadow-sm hover:text-gray-900">Sign
+                                up</a>
+                            <p class="mt-6 text-center text-base font-medium text-gray-500">
+                                Existing customer?
+                                <a href="#" class="text-indigo-600 hover:text-indigo-500">Sign in</a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="space-y-6 py-6 px-5">
+                    <div class="flex items-center gap-y-4 gap-x-8">
+                        <a href="/home" class="text-base font-medium text-gray-500 hover:text-gray-900"><span
+                                class="">Home<br><i class="fa-solid fa-house text-2xl px-2"></i></span></a>
+                        <a href="#" class="text-base font-medium text-gray-500 hover:text-gray-900"><span
+                                class="">Lojas<br><i class="fa-solid fa-shop text-2xl"></i></span></a>
+                        <a href="#" class="text-base font-medium text-gray-500 hover:text-gray-900"><span
+                                class="">Produtos<br><i class="fa-brands fa-shopify text-2xl px-4"></i></span></a>
+                        <a href="#" class="text-base font-medium text-gray-500 hover:text-gray-900"><span
+                                class="">Anúncios<br><i class="fa-solid fa-newspaper text-2xl px-4"></i></span></a>
                     </div>
                 </div>
             </div>
-            <div class="space-y-6 py-6 px-5">
-                <div class="flex items-center gap-y-4 gap-x-8">
-                    <a href="/home" class="text-base font-medium text-gray-500 hover:text-gray-900"><span
-                            class="">Home<br><i class="fa-solid fa-house text-2xl px-2"></i></span></a>
-                    <a href="#" class="text-base font-medium text-gray-500 hover:text-gray-900"><span
-                            class="">Lojas<br><i class="fa-solid fa-shop text-2xl"></i></span></a>
-                    <a href="#" class="text-base font-medium text-gray-500 hover:text-gray-900"><span
-                            class="">Produtos<br><i class="fa-brands fa-shopify text-2xl px-4"></i></span></a>
-                    <a href="#" class="text-base font-medium text-gray-500 hover:text-gray-900"><span
-                            class="">Anúncios<br><i class="fa-solid fa-newspaper text-2xl px-4"></i></span></a>
-                </div>
-            </div>
-        </div>
-    </div> --}}
+        </div> --}}
     {{--  --}}
-</div>
-<div class="mt-2">
-    <a href="#">
-        <div class="flex flex-1">
-            <h1 class="bg-yellow-300 w-44 ml-16 text-4xl font-bold text-gray-700 rounded-l">PUBLIQUE</h1>
-            <h1 class="bg-gray-700 w-46 text-4xl font-bold text-yellow-300 rounded-r">GRATIS</h1>
-
-            <div class="relative ml-36 w-96">
-                <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                    <svg aria-hidden="true" class="w-5 h-5 text-gray-700 " fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+    
+    <div class="container flex flex-wrap items-center justify-between mx-auto">
+    
+        <div class="hidden w-full md:block md:w-auto py-2">
+            <div class="flex flex-1 w-full">
+                <div class="relative ml-10">
+                    <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                        <svg aria-hidden="true" class="w-5 h-5 text-gray-700 " fill="none" stroke="currentColor"
+                            viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
+                        </svg>
+                    </div>
+                    <input type="search" id="default-search"
+                        class=" p-4 pl-10 focus:bg-white  border-b-2 focus:border-blue-600 focus:outline-none text-gray-600"
+                        placeholder="Busque roupas, calçados..." required style="width: 30rem">
+                    <button type="submit"
+                        class="text-white absolute right-2.5 bottom-2.5 bg-blue-600   font-medium rounded-lg text-sm px-4 py-2 ">Buscar</button>
                 </div>
-                <input type="search" id="default-search" class="block w-full p-4 pl-10 focus:bg-white focus:border-blue-600 focus:outline-none" placeholder="Busque roupas, calçados..." required>
-                <button type="submit" class="text-gray-700 absolute right-2.5 bottom-2.5 bg-yellow-300   font-medium rounded-lg text-sm px-4 py-2 ">Pesquisar</button>
+                <p class="absolute right-16 py-3 text-gray-500">Serviços Personalizados</p>
+                <p class="absolute right-16 py-8 text-black font-bold">(38) 9 9999-8888</p>
             </div>
-
-            <div class="relative ml-60">
-                <h1 class="ml-6 font-bold text-gray-500" >Contato</h1>
-                <h2>(00)9 9999-8888</h2>
-            </div>
-           
         </div>
-        
-    </a>
+    </div>
 </div>
+
+<script>
+    function open_menu() {
+        console.log("deu");
+    }
+</script>
--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -1,4 +1,46 @@
-<title>{{ $title }}</title>
 <div>
+    <title>{{ $title }}</title>
     @extends('layouts.navbar')
+
+    {{-- <div class="flex flex-wrap items-center justify-between m-auto pt-36 ">       
+        <div class="grid  grid-flow-col gap-4 pt-16 w-screen bg-blue-800">
+            <div class="row-span-6 bg-blue-600 w-100 ml-3 max- h-60 " >
+                <p>kfkafhkadjfhkajhfakdsjfhskadjh</p>
+            </div>
+            <div class="h-20 teste w-48" >01</div>
+            <div class="h-20 bg-red-300 w-48" >01</div>
+            <div class="h-20 bg-red-300 w-48" >01</div>
+           
+        </div>
+    </div> --}}
+
+    <div class="w-screen pt-24 md:hidden">
+        <div class="flex flex-1 w-full">
+            <div class="relative ml-10">
+                <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                    <svg aria-hidden="true" class="w-5 h-5 text-gray-700 " fill="none" stroke="currentColor"
+                        viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
+                    </svg>
+                </div>
+                <input type="search" id="default-search"
+                    class=" p-4 pl-10 focus:bg-white  border-b-2 focus:border-blue-600 focus:outline-none text-gray-600"
+                    placeholder="Busque roupas, calçados..." required style="width: 30rem">
+                <button type="submit"
+                    class="text-white absolute right-2.5 bottom-2.5 bg-blue-600   font-medium rounded-lg text-sm px-4 py-2 ">Buscar</button>
+            </div>
+            <p class="absolute right-16 py-3 text-gray-500">Serviços Personalizados</p>
+            <p class="absolute right-16 py-8 text-black font-bold">(38) 9 9999-8888</p>
+        </div>
+        <div class="main-carrocel">
+            <img src="/images/teste.jpg" alt="">
+        </div>
+        <div class="cards-box grid grid-cols-2 gap-4  pt-2">
+             <div class="bg-green-500"><h1>testes</h1></div>
+             <div class="bg-green-500"><h1>testes</h1></div>
+             <div class="bg-green-500"><h1>testes</h1></div>
+             <div class="bg-green-500"><h1>testes</h1></div>
+        </div>
+    </div>
 </div>
