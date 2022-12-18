712408bd7a02dd4500fd1e90de829e8fbb479088
thiagop127001 <thiagogua2009@gmail.com>
2022-12-18 09:14:02 -0300

    criado layout footer

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,61 @@
+<div class="footer md:hidden">
+    <div class="sociais flex items-center justify-center space-x-8 border-b-2 border-white py-2">
+        <a href="#">
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-facebook text-white"
+                width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
+                stroke-linecap="round" stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                <path d="M7 10v4h3v7h4v-7h3l1 -4h-4v-2a1 1 0 0 1 1 -1h3v-4h-3a5 5 0 0 0 -5 5v2h-3"></path>
+            </svg>
+        </a>
+        {{--  --}}
+        <a href="#">
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-instagram text-white"
+                width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
+                stroke-linecap="round" stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                <rect x="4" y="4" width="16" height="16" rx="4"></rect>
+                <circle cx="12" cy="12" r="3"></circle>
+                <line x1="16.5" y1="7.5" x2="16.5" y2="7.501"></line>
+            </svg>
+        </a>
+        {{--  --}}
+        <a href="#">
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-whatsapp text-white"
+                width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
+                stroke-linecap="round" stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                <path d="M3 21l1.65 -3.8a9 9 0 1 1 3.4 2.9l-5.05 .9"></path>
+                <path
+                    d="M9 10a0.5 .5 0 0 0 1 0v-1a0.5 .5 0 0 0 -1 0v1a5 5 0 0 0 5 5h1a0.5 .5 0 0 0 0 -1h-1a0.5 .5 0 0 0 0 1">
+                </path>
+            </svg>
+        </a>
+    </div>
+    <div class="links">
+        <a href="/" class="flex items-center justify-center mt-4 pb-4">
+            <img src="https://flowbite.com/docs/images/logo.svg" class="h-6 mr-3 sm:h-9" alt="Flowbite Logo" />
+            <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">PortalBocaiúva</span>
+        </a>
+        <p class="flex items-center justify-center text-white text-xl pb-6">O portal do comércio de Bocaiúva</p>
+        <p class="flex items-center justify-center text-white p-2">Sobre</p>
+        <p class="flex items-center justify-center text-white p-2">Serviços</p>
+        <p class="flex items-center justify-center text-white p-2">Planos de anúncios</p>
+        <p class="flex items-center justify-center text-white p-2">PortalZap</p>
+        <h1 class="flex items-center justify-center text-white text-xl p-2">Contato</h1>
+        <p class="flex items-center justify-center text-white p-2">portalbocaiuva@gmail.com</p>
+        <p class="flex items-center justify-center text-white p-2">(38) 99903-1551</p>
+        <div class="copyright border-t-2 border-white">
+            <p class="flex items-center justify-center mt-4 text-white"><svg xmlns="http://www.w3.org/2000/svg"
+                    class="icon icon-tabler icon-tabler-copyright text-white mr-2" width="15" height="15" viewBox="0 0 24 24"
+                    stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round"
+                    stroke-linejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                    <circle cx="12" cy="12" r="9"></circle>
+                    <path
+                        d="M14 9.75a3.016 3.016 0 0 0 -4.163 .173a2.993 2.993 0 0 0 0 4.154a3.016 3.016 0 0 0 4.163 .173">
+                    </path>
+                </svg> CopyRight: PortalBocaiuva.com</p>
+        </div>
+    </div>
+</div>
